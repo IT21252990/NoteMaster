207a14d848205626c43956eb1e124bc8331d2970
Kalinga Jayathilaka <kalingajayathilaka@gmail.com>
2024-03-30 10:21:38 +0530

    login page

---
+++ frontend/src/pages/LoginPage.js
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+import logo from "../images/Note Master Logo.png";
+
+
+const LoginPage = () => {
+    return (
+      <>
+        <div className=" bg-[#FFE6C7] flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+            <img
+              className="mx-auto h-24 w-auto"
+              src={logo}
+              alt="Your Company"
+            />
+            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+              Sign in to your account
+            </h2>
+          </div>
+  
+          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+            <form className="space-y-6" action="#" method="POST">
+              <div>
+                <label htmlFor="email" className="block text-sm font-medium leading-6 text-black">
+                  Email address :
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    autoComplete="email"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#454545] sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+  
+              <div>
+                <div className="flex items-center justify-between">
+                  <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                    Password :
+                  </label>
+                  <div className="text-sm">
+                    <a href="#" className="font-semibold text-[#FF6000] hover:text-[#FFA559]">
+                      Forgot password?
+                    </a>
+                  </div>
+                </div>
+                <div className="mt-2">
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    autoComplete="current-password"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#454545]  sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+  
+              <div>
+                <button
+                  type="submit"
+                  className="flex w-full justify-center rounded-md bg-[#FF6000] px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-[#FFA559] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Sign in
+                </button>
+              </div>
+            </form>
+  
+            <p className="mt-10 text-center text-sm text-[#454545]">
+              Not a member?{' '}
+              <a href="/signup" className="font-semibold ml-1 leading-6 text-[#FF6000] hover:text-[#FFA559]">
+               Register here 
+              </a>
+            </p>
+          </div>
+        </div>
+      </>
+    )
+  }
+  
+  export default LoginPage
